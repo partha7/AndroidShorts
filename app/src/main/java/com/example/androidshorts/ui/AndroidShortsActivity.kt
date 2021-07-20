ed7e87a406cb36c1df427eb91fb95e51f81ef168
partha7 <parthashum@gmail.com>
2021-07-20 08:43:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/androidshorts/ui/AndroidShortsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidshorts.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidshorts.R
+
+class AndroidShortsActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_android_shorts)
+    }
+}
